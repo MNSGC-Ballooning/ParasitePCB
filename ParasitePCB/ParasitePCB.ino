4f89fd55c7532c370a141a23cae0058f08a9025d
Andrew Van Gerpen <47224205+AndrewVanGerpen@users.noreply.github.com>
2020-02-21 15:12:02 -0600

    Add files via upload

--- ParasitePCB/ParasitePCB.ino
+++ ParasitePCB/ParasitePCB.ino
@@ -1,7 +1,6 @@
 #include <UbloxGPS.h>
 #include <TinyGPS++.h>
 #include <RelayXBee.h>
-//#include <XBee.h>
 #include <SD.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -10,75 +9,94 @@
 #include <DallasTemperature.h>
 #include <Servo.h> 
 
+
 ////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////// Pin Decleration ///////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////
 
 #define chipSelect BUILTIN_SDCARD //Should highlight if you have teensy 3.5/3.6/4.0 selected
-#define smartPin 10
-#define dallasOnePin 33
-#define dallasTwoPin 34
-#define pressureOnePin A1
-#define pressureTwoPin A2
-#define ubloxSerial Serial1
-#define xbeeSerial Serial3
-//#define rfd900Serial serialX //this line should all be commented if you aren't using an RFD900
-#define fixLED 39
-#define xbeeLED 38
-#define statusLED 37
-#define uniqueLED 36
-#define serialBAUD 9600 // when using arduino serial monitor, make sure baud rate is set to this same value
+#define smartPin 10               // The data pin on the servo motor. Must have PWM capabilities  -- PCB pin: 2
+#define dallasOnePin 33           // Data pin for the first dallas temperature sensor -- PCB pin:
+#define dallasTwoPin 34           // Data pin for the second dallas temperature sensor -- PCB pin: 
+#define pressureOnePin A1         // Data pin for the first honeywell pressure sensor -- PCB pin:
+#define pressureTwoPin A2         // Data pin for the second honeywell pressure sensor -- PCB pin:
+#define ubloxSerial Serial1       // Serial communication lines for the ublox GPS -- PCB pins: Serial1
+#define xbeeSerial Serial3        // Serial communication lines for the xbee radio -- PCB pins: Serial3
+//#define rfd900Serial serialX    //this line should all be commented if you aren't using an RFD900
+#define fixLED 39                 // LED to indicate GPS fix
+#define xbeeLED 38                // LED to indicate xbee communication
+#define statusLED 37              // LED to flash each data cycle
+#define uniqueLED 36              // 
+#define serialBAUD 9600           // when using arduino serial monitor, make sure baud rate is set to this same value
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////// Variable Decleration //////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-bool fix = false; // determines if the GPS has a lock
-bool fixLEDval; // gps status led
-
 bool rfd900 = false; // set true if you want this thing to operate with a rfd900 on serialX (declare above)
 bool ppod = true; // set true if you want this thing to operate as ppod flight computer
 bool magnetBootup = false; // set true if you want to activate flight by waving a magnet over the IMU
 bool bootup = false; // for use with the magnetometer
-bool smartRelease = false; // Releases the smart unit to deploy cubes
+
+
+/////////////////////////////// GPS related variables /////////////////////////////////////
+
+bool fix = false; // determines if the GPS has a lock
+unsigned long int fixTimer = 0;
+
+
+////////////////////////////// Radio related variables ///////////////////////////////////
+
 bool commandRelease = false;
-int smartReleasePosition = 180;
-int smartInitialPosition = 0;
+String groundCommand;
+String xbeeID = "PPOD";
+bool xbeeAlternation = false; // this will allow the xbee to alternate between sending data to MOC SOC and data via MOC SOC to the ground
+unsigned long int xbeeTimer = 0;
+int xbeeRate = 10000; // 10000 millis = 10 seconds
+
+String xbeeMessage; // This saves all xbee transmissions and appends them to the data string
+
+
+//////////////////////////// SMART related variables ////////////////////////////////////
+
+bool smartRelease = false; // Releases the smart unit to deploy cubes
+float cutTime = 70*60000.0; // x (minutes) * 60000;
+float cutAltitude = 50000; // 50,000 ft ASL cut!!
+int smartReleasePosition = 180; // the angle in degrees for smart release. Will vary based on smart unit
+int smartInitialPosition = 0; // the angle in degrees for smart initial position. Will vary based on smart unit
+bool smartReleaseTransmission = true; // This ensures only one message is relayed to the ground after release has occurred.
+String commandMessage; // Appends a message stating the radio deployed the cubes if that happens
+
+
+///////////////////// SD and Datalogging related variables /////////////////////////////
 
 File datalog;
 char filename[] = "SDPPOD00.csv"; //File name template
 bool sdActive = false;
 
-String header = "GPS Time, Lat, Lon, Alt (ft), AltEstimation (ft), dallas1(F), dallas2(F), pressure1 (PSI), pressure2 (PSI), release?,  time since bootup (s)";
+String header = "GPS Time, Lat, Lon, Alt (ft), AltEstimation (ft), dallas1(F), dallas2(F), pressure1 (PSI), pressure2 (PSI), release?, time since bootup (s), recent xbee message";
 String data;
 String groundData;
-String groundCommand;
-String xbeeID = "PPOD";
-
-unsigned long int fixTimer = 0;
 unsigned long int dataTimer = 0;
-unsigned long int xbeeTimer = 0;
-int xbeeRate = 10000; // 10000 millis = 10 seconds
 int dataRate = 1000; // 1000 millis = 1 second
 
 String exclamation = "!"; // this needs to be at the end of every 
 String mocSocID = "SOC?";
 String mocSocRelay = "$P$";
 
-int analogResolutionBits = 16;
+int analogResolutionBits = 12;
 int analogResolutionVals = pow(2,analogResolutionBits);
-
 float pressureBoundary1;
 float pressureBoundary2;
 float pressureBoundary3;
-float dallasOneF;
-float dallasTwoF;
 float pressureOnePSI;
 float pressureTwoPSI;
 float altitudeFt;
 
-float cutTime = 70*60000.0; // x (minutes) * 60000;
-float cutAltitude = 50000; // 50,000 ft ASL cut!!
+float dallasOneF;
+float dallasTwoF;
 
 float magnetometer[3]; // {x, y, z}
 float accelerometer[3]; // {x, y, z}
@@ -97,6 +115,7 @@ OneWire oneWireOne(dallasOnePin); //For Dallas sensor
 OneWire oneWireTwo(dallasTwoPin); //For Dallas sensor
 DallasTemperature dallasOne(&oneWireOne); //For Dallas sensor
 DallasTemperature dallasTwo(&oneWireTwo); //For Dallas sensor
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////// Function Decleration //////////////////////////////////////
@@ -121,6 +140,11 @@ void updateData();
 void blinkSequence();
 void Blink(int led, bool ledVal);
 
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// SETUP /////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+
 void setup() {
   
   Serial.begin(serialBAUD); //define baud rate in variable decleration above
@@ -142,11 +166,20 @@ void setup() {
   logData(header);
 }
 
+
+////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////// LOOP ////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   updateData();
   
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// Setup Functions  //////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
 
 void sdSetup(){
   pinMode(chipSelect,OUTPUT);
@@ -169,6 +202,7 @@ void sdSetup(){
   }
 }
 
+
 void xbeeSetup(){
   xbeeSerial.begin(XBEE_BAUD);
   xbee.init('A'); // Need to make sure xbees on both ends have the same identifier. "AAAA"
@@ -178,6 +212,7 @@ void xbeeSetup(){
   xbee.exitATmode();
   Serial.println("Xbee initialized");
 }
+
 
 void ubloxSetup(){
   ubloxSerial.begin(UBLOX_BAUD);
@@ -194,10 +229,12 @@ void ubloxSetup(){
   }
 }
 
+
 void imuSetup(){
   Wire.begin();
   if(!imu.begin()) Serial.println("Failed to communicate with LSM9DS1.");
 }
+
 
 void pressureToAltitudeSetup()
 {
@@ -211,14 +248,17 @@ void pressureToAltitudeSetup()
   pressureBoundary3 = (51.97*pow(((T3 + 459.7)/389.98),-11.388));
 }
 
+
 void rfd900Setup(){
   ;// code
 }
+
 
 void ppodSetup(){
   smart.attach(smartPin);
   smart.write(smartInitialPosition);
 }
+
 
 void magnetometerBootup(){
   if( magnetometer[0] > 4.0 || magnetometer[1] > 4.0 || magnetometer[2] > 4.0 ) bootup = true;
@@ -226,12 +266,14 @@ void magnetometerBootup(){
   // code
 }
 
+
 void logData(String Data){
   datalog = SD.open(filename, FILE_WRITE);
   datalog.println(Data);
   datalog.close();
   Serial.println(Data);
 }
+
 
 void updateXbee(){ // This is disgusting
   
@@ -248,70 +290,79 @@ void updateXbee(){ // This is disgusting
   // M      Polo! just a ping command
 
   if(millis() - xbeeTimer > xbeeRate){
+    if(xbeeAlternation) xbeeAlternation = false;
+    else{ xbeeAlternation = true; }// This is so each xbeeRate cycle can alternate between sending data and
+    
     xbeeTimer = millis();
-    xbeeSerial.print(mocSocID + "R" + String(smartRelease) + exclamation); // This would be in the format "SOC?R0!" Where the 0 is a boolean value -- 0 or 1 -- where 1 indicates cube deployment
-    delay(100);
-    xbeeSerial.print(mocSocID + mocSocRelay + groundData + exclamation);
+    if(xbeeAlternation) xbeeSerial.print(mocSocID + "R" + String(smartRelease) + exclamation); // This would be in the format "SOC?R0!" Where the 0 is a boolean value -- 0 or 1 -- where 1 indicates cube deployment
+    
+    else{ xbeeSerial.print(mocSocID + mocSocRelay + groundData + exclamation); } // This sends data through MOC-SOC to the ground
   }
   
   if (xbeeSerial.available() > 0) {
     //groundCommand = xbeeSerial.readString(); //for troubleshooting only
     groundCommand = xbee.receive();
-    logData(", Command: " + groundCommand);
 
     if(groundCommand.startsWith("T"))
     {
-      xbee.send(mocSocID + String(millis()-cutTime) + exclamation);
+      xbeeMessage = (mocSocID + mocSocRelay + String(cutTime-millis()) + exclamation);
     }
     else if(groundCommand.startsWith("+"))
     {
       groundCommand.remove(0,1);
       float timeAdded = groundCommand.toFloat();
       cutTime = cutTime + timeAdded;
+      xbeeMessage = (mocSocID + mocSocRelay + "New cut time: " + String(cutTime) + exclamation);
     }
     else if(groundCommand.startsWith("-"))
     {
       groundCommand.remove(0,1);
       float timeSubtracted = groundCommand.toFloat();
       cutTime = cutTime - timeSubtracted;
+      xbeeMessage = (mocSocID + mocSocRelay + "New cut time: " + String(cutTime) + exclamation);
     }
     else if(groundCommand.startsWith("A"))
     {
-      xbee.send(mocSocID + mocSocRelay + "Altitude: " + String(altitudeFt) + exclamation);
+      xbeeMessage = (mocSocID + mocSocRelay + "Altitude calculated from pressure: " + String(altitudeFt) + exclamation);
     }
     else if(groundCommand.startsWith("A+"))
     {
       groundCommand.remove(0,1);
       float altitudeAdded = groundCommand.toFloat();
       cutAltitude = cutAltitude + altitudeAdded;
+      xbeeMessage = (mocSocID + mocSocRelay + "New cut altitude: " + String(cutAltitude) + exclamation);
     }
     else if(groundCommand.startsWith("A-"))
     {
       groundCommand.remove(0,1);
       float altitudeSubtracted = groundCommand.toFloat();
       cutAltitude = cutAltitude - altitudeSubtracted;
-      xbee.send(mocSocID + mocSocRelay + "New Altitude: " + String(altitudeFt) + exclamation);
+      xbeeMessage = (mocSocID + mocSocRelay + "New cut altitude: " + String(cutAltitude) + exclamation);
     }
     else if(groundCommand.startsWith("C"))
     {
-      if(smartRelease) xbee.send("cubes deployed prior to command");
+      if(smartRelease) xbee.send(mocSocID + mocSocRelay + "Cubes deployed prior to command" + exclamation);
       else{ smartRelease = true;
             commandRelease = true;
       }
     }
     else if(groundCommand.startsWith("D"))
     {
-      xbee.send(data);
+      xbeeMessage = (mocSocID + mocSocRelay + data + exclamation);
     }
     else if(groundCommand.startsWith("M"))
     {
-      xbee.send("Polo");
+      xbeeMessage = (mocSocID + mocSocRelay + "Polo" + exclamation);
     }
+    else{
+      xbeeMessage = (mocSocID + mocSocRelay + "Error - command not recognized: " + groundCommand + exclamation);
+    }
+    xbee.send(xbeeMessage);
   }
 }
 
+
 void updateIMU(){
-  
   if( imu.gyroAvailable() ) imu.readGyro();
   if( imu.accelAvailable() ) imu.readAccel();
   if( imu.magAvailable() ) imu.readMag();
@@ -327,6 +378,7 @@ void updateIMU(){
   gyroscope[2] = imu.calcGyro(imu.gz);
 }
 
+
 void updatePressure() { // Output units: psi -- far from efficient, but works for our purpose
   analogReadResolution(analogResolutionBits);
   float rawPressure = analogRead(pressureOnePin);
@@ -340,8 +392,8 @@ void updatePressure() { // Output units: psi -- far from efficient, but works fo
   pressureTwoPSI = pressure;
 }
 
-void pressureToAltitude(){ // Output units: feet
 
+void pressureToAltitude(){
   float pressurePSF = (pressureOnePSI*144);
   
   float altFt = -1.0;
@@ -360,6 +412,7 @@ void pressureToAltitude(){ // Output units: feet
   altitudeFt = altFt;
 }
 
+
 void updateDallas(){ // This looks different from pressure retreival because it is digital, not analog
   dallasOne.requestTemperatures();
   dallasTwo.requestTemperatures();
@@ -367,9 +420,9 @@ void updateDallas(){ // This looks different from pressure retreival because it 
   dallasTwoF = dallasTwo.getTempFByIndex(0);
 }
 
+
 void updateData(){
   ublox.update();
-  //Serial.println("test"); // for prototyping
   blinkSequence();
   updateXbee();
   
@@ -384,7 +437,7 @@ void updateData(){
     data = String(ublox.getHour()-5) + "," + String(ublox.getMinute()) + "," + String(ublox.getSecond()) + ", "
            + String(ublox.getLat(), 4) + ", " + String(ublox.getLon(), 4) + ", " + String(ublox.getAlt_feet(), 4)
            +  ", " + String(altitudeFt) + "," + String(dallasOneF) + ", " + String(dallasTwoF) + ", "
-           + String(pressureOnePSI) + ", " + String(pressureTwoPSI) + ", " + String(smartRelease) + ", " + String(millis());
+           + String(pressureOnePSI) + ", " + String(pressureTwoPSI) + ", " + String(smartRelease) + ", " + String(millis()) + ", " + xbeeMessage;
 
     groundData = "GPS altitude (ft):" + String(ublox.getAlt_feet(),4) + ", Estimated Altitude(ft):" + String(altitudeFt)
                   + ", millis(): " + String(millis()) + ", cutTime: " + String(cutTime);
@@ -393,8 +446,10 @@ void updateData(){
     else fix = true;
 
     logData(data);
+    xbeeMessage = "";
   }         
 }
+
 
 void updateSmart()
 {
@@ -405,38 +460,40 @@ void updateSmart()
   
   if(smartRelease){
     smart.write(smartReleasePosition);
-    if(commandRelease)xbee.send("cubes deployed via command");
-    else{xbee.send("cubes deployed! Altitude(ft): " + String(altitudeFt) + "; Time = " + String(millis()));}
+    
+    if(commandRelease){
+      commandMessage = " via command";
+      commandRelease = false;
+    }
+    
+    if(smartReleaseTransmission){
+      xbeeMessage = (mocSocID + mocSocRelay + "cubes deployed" + commandMessage + "! Altitude(ft): " + String(altitudeFt) + "; Time = " + String(millis()) + exclamation);
+      smartReleaseTransmission = false;
+      xbee.send(xbeeMessage);
+    }
   }
 }
+
 
 void blinkSequence()
 {
   if(fix){
     if(millis()-fixTimer > 1000){
       fixTimer = millis();
-      Blink(fixLED,fixLEDval);
+      //Blink(fixLED,fixLED);
     }
   }
   else{
     if(millis()-fixTimer > 200){
       fixTimer = millis();
-      Blink(fixLED,fixLEDval);
+      //Blink(fixLED,fixLEDval);
     }
   }
 }
+
 
 void Blink(int led, bool ledVal)
 {
   ledVal = !ledVal;
   digitalWrite(led,ledVal);
 }
-
-/* functionality notes:
-    Good idea: Set boolean values to true if certain things need to happen
-    1) hook up RFD or other radio operating with serial
-    2) operate as a ppod vs Parasitic logger
-    3) bootup using a magnet
-    4) operate as a ppod
-
-*/
